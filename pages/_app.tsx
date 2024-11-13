0caa8351554b7915a8ab45fba1021f3338476eac
konstantin.kolesnik <Konstan.Kolesnik@x5.ru>
2024-11-14 01:11:35 +0300

    add SpeedInsights

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,14 @@ import '@/styles/globals.scss'
 import type { AppProps } from 'next/app'
 
 import { MainLayout } from "@/components/MainLayout";
+import { SpeedInsights } from '@vercel/speed-insights/next'; // FIXME от vercel
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Component {...pageProps} />
+    <>
+      <Component {...pageProps} />
+      <SpeedInsights />
+    </>
   )
 }
 
