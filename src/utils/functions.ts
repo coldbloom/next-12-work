11f6db4dd2e3ca5eb9fe14f2db38debb265e7e8b
konstantin.kolesnik <Konstan.Kolesnik@x5.ru>
2025-03-24 01:34:07 +0300

    some clear

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -57,10 +57,6 @@ export function secondsToHoursMinutes(seconds: number): string {
   return result;
 }
 
-
-
-type TPhone = string | null | undefined;
-
 export function formatPhone(phone: string | null | undefined): string | null | undefined {
   // Форматируем номер
   if (phone) {
@@ -69,48 +65,3 @@ export function formatPhone(phone: string | null | undefined): string | null | u
     return undefined;
   }
 }
-
-
-// export const lowerCaseFirstLetter = (str: string): string => str[0].toLowerCase() + str.slice(1);
-//
-// // эта функция не экспортируется
-// const formatParentName = (parent: ParentLocation): string => {
-//   const restrictedNames = new Set(["Донецкая Народная", "Луганская Народная"]);
-//
-//   if (restrictedNames.has(parent.name)) {
-//     return `${parent.name} ${parent.type}`;
-//   }
-//
-//   return (parent.type === 'Республика' || parent.type === 'Город')
-//     ? `${parent.type} ${parent.name}`
-//     : `${parent.name} ${lowerCaseFirstLetter(parent.type)}`;
-// };
-//
-// export const formatComma = (parents: ParentLocation[]): string => {
-//   // Используем reduce для создания итоговой строки
-//   return parents.reduce((acc, parent, index) => {
-//     const prefix = index > 0 ? ', ' : ''; // Запятая перед элементом, кроме первого
-//     return acc + prefix + formatParentName(parent); // Конкатенируем предыдущую строку и новый элемент
-//   }, ''); // Начальная строка пустая
-// };
-//
-//
-//
-// export function extractLocationData(data: any): Location {
-//   return {
-//     id: data.id,
-//     zip: data.zip,
-//     type: data.type,
-//     typeShort: data.typeShort,
-//     contentType: data.contentType,
-//     name: data.name,
-//     parents: data.parents.map((parent: any) => ({
-//       id: parent.id,
-//       zip: parent.zip,
-//       type: parent.type,
-//       typeShort: parent.typeShort,
-//       contentType: data.contentType,
-//       name: parent.name,
-//     }))
-//   };
-// }
